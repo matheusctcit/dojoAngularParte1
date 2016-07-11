568a18bef4f8ad67430b99453674bf2f570c15f7
matheusct <matheusct@ciandt.com>
2016-07-11 09:10:32 -0300

    first

---
+++ js/controller/branchs.controller.js
@@ -0,0 +1,23 @@
+//criar branchsCtrl
+	
+	// branchs = [
+	// 	{branch: 'fase2/sprint9/CD-456', origem: 'stage', data: dateFilter(new Date(), 'dd/MM/yyyy HH:mm'), status: {descricao: 'pull request', ordem: 4}},
+	// 	{branch: 'fase3/sprint10/CD-1234', origem: 'stage', data: dateFilter(new Date(), 'dd/MM/yyyy HH:mm'), status: {descricao: 'develop', ordem: 1}},
+	// 	{branch: 'fase1/sprint07/CD-89', origem: 'master', data: dateFilter(new Date(), 'dd/MM/yyyy HH:mm'), status: {descricao: 'done', ordem: 5}}
+	// ];
+	
+	// statusList = [
+	// 	{descricao: 'develop', ordem: 1},
+	// 	{descricao: 'test', ordem: 2},
+	// 	{descricao: 'tested', ordem: 3},
+	// 	{descricao: 'pull request', ordem: 4},
+	// 	{descricao: 'done', ordem: 5}
+	// ];
+	
+	// function adicionarBranch
+	
+	// function apagarBranchs
+	
+	// function isBranchSelecionado
+	
+	//  function ordenarPor
